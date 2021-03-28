6f9ab07857b796133518d2d9903ac259d71a5de0
Yens Van Hout <vanhoutyens@gmail.com>
2021-03-28 01:02:15 +0100

    js in seperate files

---
+++ js/timer.js
@@ -0,0 +1,42 @@
+let timerbar = document.querySelector(".timerforeground");
+        let timertext = document.querySelector(".timertext");
+        let scoretext = document.querySelector(".score");
+
+        let time = 121;
+
+        let totalscore;
+        let score = 0;
+        let outOf = 0;
+
+        setInterval(() => {
+            if (time > 1) {
+
+                time--;
+
+                let width = (100 / 120 * time).toFixed(1);
+
+                let minutes = Math.floor(time / 60);
+                let seconds = time - minutes * 60;
+
+                if (seconds < 10) {
+                    seconds = "0" + seconds;
+                }
+
+                let remainder = minutes + ":" + seconds;
+
+                timertext.innerHTML = remainder;
+                timerbar.style.width = width + "%";
+
+            } else {
+                //reset to default values
+                time = 121;
+                width = 100;
+
+                let randomgen = Math.random() < 0.5 ? 0 : 1;
+
+                score = score + randomgen;
+                outOf++;
+                scoretext.innerHTML = score + "/" + outOf;
+
+            }
+        }, 1000);
