3eb619becae7639ecbd1b746c955927a3f217146
Yens Van Hout <vanhoutyens@gmail.com>
2021-03-28 19:40:50 +0200

    clean-up and center text menu mobile

--- js/helpandclose.js
+++ js/helpandclose.js
@@ -1,9 +1,9 @@
-let help = document.querySelector(".help");
+let help = document.querySelector(".help")
 
 help.addEventListener("click", function () {
-    let quote = document.querySelector(".quoteOutput").textContent;
-    window.open('http://google.com/search?q=' + quote);
+    let quote = document.querySelector(".quoteOutput").textContent
+    window.open('http://google.com/search?q=' + quote)
 })
 
-let close = document.querySelector(".close");
-close.addEventListener("click", () => window.close());
+let close = document.querySelector(".close")
+close.addEventListener("click", () => window.close())
--- js/timer.js
+++ js/timer.js
@@ -1,42 +1,43 @@
-let timerbar = document.querySelector(".timerforeground");
-        let timertext = document.querySelector(".timertext");
-        let scoretext = document.querySelector(".score");
+        let timerbar = document.querySelector(".timerforeground")
+        let timertext = document.querySelector(".timertext")
+        let scoretext = document.querySelector(".score")
 
-        let time = 121;
+        let time = 121
 
-        let totalscore;
-        let score = 0;
-        let outOf = 0;
+        let score = 0
+        let outOf = 0
 
         setInterval(() => {
             if (time > 1) {
 
-                time--;
+                time--
 
-                let width = (100 / 120 * time).toFixed(1);
+                let width = (100 / 120 * time).toFixed(1)
 
-                let minutes = Math.floor(time / 60);
-                let seconds = time - minutes * 60;
+                let minutes = Math.floor(time / 60)
+                let seconds = time - minutes * 60
 
                 if (seconds < 10) {
-                    seconds = "0" + seconds;
+                    seconds = "0" + seconds
                 }
 
-                let remainder = minutes + ":" + seconds;
+                let remainder = minutes + ":" + seconds
 
-                timertext.innerHTML = remainder;
-                timerbar.style.width = width + "%";
+                timertext.innerHTML = remainder
+                timerbar.style.width = width + "%"
 
             } else {
                 //reset to default values
-                time = 121;
-                width = 100;
+                time = 121
+                width = 100
 
-                let randomgen = Math.random() < 0.5 ? 0 : 1;
+                //dummy to check score changes
+                let randomgen = Math.random() < 0.5 ? 0 : 1
 
-                score = score + randomgen;
-                outOf++;
-                scoretext.innerHTML = score + "/" + outOf;
+                score = score + randomgen
+                outOf++
+                
+                scoretext.innerHTML = score + "/" + outOf
 
             }
-        }, 1000);
+        }, 1000)
