ce2d824e6ccff927278446411b0a5dd833ebaef8
Broddin Jens <broddin.jens>
2021-03-29 21:58:43 +0200

    cleanup

--- js/helpandclose.js
+++
@@ -1,9 +0,0 @@
-let help = document.querySelector(".help")
-
-help.addEventListener("click", function () {
-    let quote = document.querySelector(".quoteOutput").textContent
-    window.open('http://google.com/search?q=' + quote)
-})
-
-let close = document.querySelector(".close")
-close.addEventListener("click", () => window.close())
