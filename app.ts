313d80f84fa00f1b3b9aae111cf64502d4b903d6
Yens Van Hout <vanhoutyens@gmail.com>
2021-05-03 17:40:58 +0200

    HEX GEN + START API FETCH TS

--- app.ts
+++ app.ts
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+const fetch = require('node-fetch');
+
+const baseurl : string = "https://the-one-api.dev/v2";
+
+const quoteurl : string = "/quote/";
+const characterurl : string = "/character/";
+const movieurl : string = "/movie/";
+
+const frighten : string = 'aXaL57Oa7s0-QqAEoz1C';
+
+let quote : any = {};
 
 
 //Deze lijn zorgt er voor dat de server de files opvraagt via de public folder.
@@ -24,7 +35,38 @@ app.get('/verificationlr',(req:any,res:any)=>{
 })
 
 app.get('/game',(req:any,res:any)=>{
+    
+    //Genereer een geldig ID voor de fetch
+    //TODO: SIMPLIFY HEX GEN
+    let random : string = (Math.floor((Math.random() * (5408 - 3017)) + 3017)).toString();
+    let random_base : string = '2873547737497751740221154';
+    
+    let random_id_dec : string = (random_base + random);
+    let random_id_big : bigint = BigInt(random_id_dec);
+    let random_id_hex : string = random_id_big.toString(16);
+    
+    //console.log(random_id_dec);
+    //console.log(random_id_big);
+    //console.log(random_id_hex);
+    //console.log("--------------------------------");
+
+    //TODO: FIX THIS MESS
+
+    let promise1 = fetch((baseurl + quoteurl + random_id_hex),
+    {
+        method: 'GET',
+        headers: {
+            'Accept': '*/*',
+            'Authorization': 'Bearer ' + frighten
+        }}).then((response : any) => {
+        quote = response.json();
+    }).then((response : any) =>{
+        console.log(quote);
+    });
+
+
     res.render('game');
+
 })
 
 app.get('/notes',(req:any,res:any)=>{
@@ -56,3 +98,5 @@ app.use((req:any, res:any) => {
 //Zorgt ervoor dat de server blijft luisteren naar jou requests.
 app.listen(app.get('port'), 
     ()=>console.log( '[server] http://localhost:' + app.get('port')));
+
+    export{}
